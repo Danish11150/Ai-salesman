fc31759f1d1e0edbcde637c0c99f26983cf4471f
Danish11150 <danishmughalkids@gmail.com>
2026-05-30 00:30:27 +0300

    Clean up imports in main.py
    Removed unused imports and code related to sys.path.

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import json
 import requests
 from flask import Flask, request, jsonify
 
-import sys
-
-# 🔹 Add current directory to Python path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from website import website
 
 from lan.language import LANGUAGE_SYSTEM_PROMPT
 from assets.personality import AI_PERSONALITY_PROMPT
