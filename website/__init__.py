5f3622c6bacce6209fade03f712eb07b5e7cdc65
Danish11150 <danishmughalkids@gmail.com>
2026-05-30 17:29:03 +0300

    Initialize Flask blueprint for website

---
+++ website/__init__.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+website = Blueprint(
+    "website",
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
+
+@website.route("/")
+def home():
+    return render_template("index.html")
